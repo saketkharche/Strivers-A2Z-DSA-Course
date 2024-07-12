bdf0a9e08922dfad9ecb29714bf569de057d0f18
Saket M Kharche <kharchesaket@gmail.com>
2024-07-12 15:50:31 +0530

    practise

---
+++ Step01-Learn the basic/Lec 1/user input/input-output.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <ostream>
+using namespace std;
+int main() {
+  int x, y;
+  // std::cout << "Hey Saket!" << "\n";
+  // std::cout << "heyy saket";
+  // std::cout << "heyy saket" << std::endl << "hey";
+  cin >> x >> y;
+  cout << "value of x :" << x <<"and y:"<<y;
+  return 0;
+}
