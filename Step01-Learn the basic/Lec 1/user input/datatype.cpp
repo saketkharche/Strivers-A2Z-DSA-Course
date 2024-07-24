c5f68b146ea9f4936d4a24ef37e54550a30a63ed
Saket M Kharche <kharchesaket@gmail.com>
2024-07-24 12:43:33 +0530

    2024-07-24 12:43:42

---
+++ Step01-Learn the basic/Lec 1/user input/datatype.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // Integer: The keyword used for integer data types is int.
+    // Integers typically require 4 bytes of memory space and
+    // range from -2147483648 to 2147483647
+
+    // int x = 10;
+
+    // //long
+    // long x=13;
+    // cin >> x;
+
+    // long long x = 784521;
+
+    //float and double
+    // float x=5;
+    // float y=8.6;
+    // cout << "valusse of y :"<<y;
+    
+    //string and getline
+    // string s1 ;
+    // string s2;
+
+    // cin >> s1 >> s2;
+    // cout<<s1<<s2;
+
+    // string str;
+    // getline(cin,str);
+    // cout<< str;
+
+    char ch;
+    cin >> ch;
+    cout << ch;
+    
+    
+    return 0;
+
+}
