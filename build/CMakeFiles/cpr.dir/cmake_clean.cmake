63ee34b3b28912c5460e55365092e7982a7be708
Saket M Kharche <kharchesaket@gmail.com>
2025-01-17 17:35:27 +0530

    commit

---
+++ build/CMakeFiles/cpr.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpr.dir/send_post_request.obj"
+  "CMakeFiles/cpr.dir/send_post_request.obj.d"
+  "libcpr.dll"
+  "libcpr.dll.a"
+  "libcpr.dll.manifest"
+  "libcpr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
